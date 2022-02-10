22e43d867a5f785994db2bccb02bfe8a025e0017
Dragosh Gheceanu <dragoshcode@gmail.com>
2022-02-10 13:03:42 +0200

    Add logic for setting todo as Done

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -2,17 +2,29 @@ import React from 'react';
 
 function TodoItem({ text, todos, setTodos, todo }) {
   const handleDelete = () => {
-    setTodos(todos.filter((e) => e.id !== todo.id));
+    setTodos(todos.filter((el) => el.id !== todo.id));
   };
-  const handleComplete
+  const handleComplete = () => {
+    setTodos(
+      todos.map((el) => {
+        if (todo.id === el.id) {
+          return {
+            ...todo,
+            done: !el.done,
+          };
+        }
+        return el;
+      })
+    );
+  };
 
   return (
     <div className='flex justify-between items-center bg-white gap-4 mt-3'>
       <div>
-        <li className='md:px-8'>{text}</li>
+        <li className={`md:px-8 ${todo.done ? 'done' : ''}`}>{text}</li>
       </div>
       <div className='flex'>
-        <button className='bg-green-400 p-2'>
+        <button onClick={handleComplete} className='bg-green-400 p-2'>
           <i class='fa-solid fa-check text-white pointer-events-none'></i>
         </button>
         <button onClick={handleDelete} className='bg-red-400 p-2'>
